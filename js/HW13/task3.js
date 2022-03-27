c107d6e548bbffff7835fdadf9d43f981ca7d291
dronovs <od270891dss@gmail.com>
2022-03-27 20:37:22 +0300

    update

--- js/HW13/task3.js
+++ js/HW13/task3.js
@@ -1,20 +1,23 @@
 'use strict'
 
 let numOne = prompt('Введите целое число');
+let result = 0;
 
 if (+numOne > 0) {
-    console.log(`Число ${numOne} положительное`);
+    result = `Число ${numOne} положительное`;
 } else {
-    console.log(`Число ${numOne} отрицательное`);
+    result = `Число ${numOne} отрицательное`;;
 }
 if (numOne.length === 1) {
-    console.log(`Число ${numOne} однозначное`);
+    result += ` однозначное`;
 } else if (numOne.length === 2) {
-    console.log(`Число ${numOne} двузначное`);
+    result += ` двузначное`;
 } else if (numOne.length === 3) {
-    console.log(`Число ${numOne} трехзначное`);
+    result += ` трехзначное`;
 } else if (numOne.length > 3) {
-    console.log(`Число ${numOne} многозначное`);
+    result += ` многозначное`;
 } else {
-    console.log(`Invalid value`);
+    result += ` Invalid value`;
 }
+
+console.log( result );
